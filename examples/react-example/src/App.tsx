cc79719d1b93446882f73b8b1da0a695340156cb
Donald Onyino <58218526+porkytheblack@users.noreply.github.com>
2023-07-16 12:40:33 +0300

    chore: updated component usage

--- examples/react-example/src/App.tsx
+++ examples/react-example/src/App.tsx
@@ -7,11 +7,14 @@ function App() {
   return (
     <>
       <PaymentForm
-        amount={1000}
+        amount={1000} // amount in KES
         onFormSubmit={(data)=>{
-          console.log(data)
+          console.log(data) // this is where you handle the form data
         }}
         additional_fields={{
+          // by default this can be left empty and the phone number will be the only field
+          // you can add more fields here and they will be added to the form
+          // pass in the default value if you already have the data
           first_name: {
             default_value: "Joe"
           },
