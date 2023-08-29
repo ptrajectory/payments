fda60da5896d6a7da16ece8f6579fabf29a2bb16
Donald Onyino <58218526+porkytheblack@users.noreply.github.com>
2023-08-29 09:28:35 +0300

    chore: setup tsup config

---
+++ utils/functions/tsup.config.ts
@@ -0,0 +1,17 @@
+import { defineConfig } from 'tsup'
+
+export default defineConfig((opts)=>{
+    return {
+        entry: ["./index.ts", "./schema.ts"],
+        splitting: false,
+        sourcemap: true,
+        clean: !opts.watch,
+        dts: true,
+        format: ["esm"],
+        ignoreWatch: [
+            "**/node_modules/**",
+            "**/.git/**",
+            "**/dist/**",   
+        ]
+    }
+})
